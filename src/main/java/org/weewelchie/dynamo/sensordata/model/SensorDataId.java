e8bd40025107678f3c75dcee363d2503934b34e5
welchie <chris@weewelchie.org>
2023-07-25 23:33:27 +0100

    Initial Version for SensorData DynamoDB Project (spring-boot)

---
+++ src/main/java/org/weewelchie/dynamo/sensordata/model/SensorDataId.java
@@ -0,0 +1,35 @@
+package org.weewelchie.dynamo.sensordata.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+
+import java.io.Serializable;
+
+public class SensorDataId implements Serializable {
+
+
+
+    private String id;
+
+    private String date;
+
+   @DynamoDBHashKey
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @DynamoDBRangeKey
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+}
---
+++ src/test/java/org/weewelchie/dynamo/sensordata/SensorDataRepositoryIntegrationTest.java
@@ -0,0 +1,134 @@
+package org.weewelchie.dynamo.sensordata;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ResourceInUseException;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.weewelchie.dynamo.sensordata.repositories.SensorDataRepository;
+import org.weewelchie.dynamo.sensordata.rule.LocalDbCreationRule;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+@ActiveProfiles("local")
+@TestPropertySource(properties = {
+        "amazon.dynamodb.endpoint=http://localhost:8000/",
+        "amazon.aws.accesskey=test1",
+        "amazon.aws.secretkey=test231" })
+public class SensorDataRepositoryIntegrationTest {
+
+    @ClassRule
+    public static LocalDbCreationRule dynamoDB = new LocalDbCreationRule();
+    private static DynamoDBMapper dynamoDBMapper;
+    private static AmazonDynamoDB amazonDynamoDB;
+
+    @Autowired
+    SensorDataRepository repository;
+
+    private static final String DYNAMODB_ENDPOINT = "amazon.dynamodb.endpoint";
+    private static final String AWS_ACCESSKEY = "amazon.aws.accesskey";
+    private static final String AWS_SECRETKEY = "amazon.aws.secretkey";
+    private static final String sensorID = "TEMP_987654321";
+    private static final String date = "2023-07-22 12:23:45";
+
+    private static final String tempC = "12.12";
+
+    private static final String tempF = "68.453";
+    private static final String info = "{ 'temp_c':12.12,'temp_f':68.453}";
+
+    @BeforeClass
+    public static void setupClass() {
+        Properties testProperties = loadFromFileInClasspath("application.properties")
+                .filter(properties -> !isEmpty(properties.getProperty(AWS_ACCESSKEY)))
+                .filter(properties -> !isEmpty(properties.getProperty(AWS_SECRETKEY)))
+                .filter(properties -> !isEmpty(properties.getProperty(DYNAMODB_ENDPOINT)))
+                .orElseThrow(() -> new RuntimeException("Unable to get all of the required test property values"));
+
+        String amazonAWSAccessKey = testProperties.getProperty(AWS_ACCESSKEY);
+        String amazonAWSSecretKey = testProperties.getProperty(AWS_SECRETKEY);
+        String amazonDynamoDBEndpoint = testProperties.getProperty(DYNAMODB_ENDPOINT);
+
+        amazonDynamoDB = new AmazonDynamoDBClient(new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey));
+        amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+        dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+    }
+    @Before
+    public void setup() throws Exception {
+
+        try {
+            dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+
+            CreateTableRequest tableRequest = dynamoDBMapper.generateCreateTableRequest(SensorData.class);
+
+            tableRequest.setProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
+
+            amazonDynamoDB.createTable(tableRequest);
+        } catch (ResourceInUseException e) {
+            // Do nothing, table already created
+        }
+
+        // TODO How to handle different environments. i.e. AVOID deleting all entries in ProductInfo on table
+        dynamoDBMapper.batchDelete((List<SensorData>) repository.findAll());
+    }
+
+    @Ignore
+    @Test
+    public void givenItemWithExpectedCost_whenRunFindAll_thenItemIsFound() {
+        SensorData sensorData = new SensorData(sensorID,date,tempC,tempF);
+        repository.save(sensorData);
+        List<SensorData> result = (List<SensorData>) repository.findAll();
+
+        assertThat(result.size(), is(greaterThan(0)));
+        assertThat(result.get(0).getDate(), is(equalTo(date)));
+    }
+
+
+    private static boolean isEmpty(String inputString) {
+        return inputString == null || "".equals(inputString);
+    }
+
+    private static Optional<Properties> loadFromFileInClasspath(String fileName) {
+        InputStream stream = null;
+        try {
+            Properties config = new Properties();
+            Path configLocation = Paths.get(ClassLoader.getSystemResource(fileName).toURI());
+            stream = Files.newInputStream(configLocation);
+            config.load(stream);
+            return Optional.of(config);
+        } catch (Exception e) {
+            return Optional.empty();
+        } finally {
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+}
